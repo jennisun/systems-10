734035ce6c815c26574f962bfdb4a6cd226557e3
jennisun <jsun20@stuy.edu>
2021-10-17 10:18:20 -0400

    insert_front

--- main.c
+++ main.c
@@ -13,10 +13,11 @@ void printcookies(struct cookies *c) {
   printf("Today, I ate %d %s cookies\n", c -> num, c -> type);
 }
 
-struct cookies * makecookies(char *t, int n) {
+struct cookies * makecookies(char *t, int n, struct cookies *s) {
   struct cookies *c = malloc(sizeof(struct cookies));
   strcpy(c -> type, t);
   c -> num = n;
+  c -> next = s;
   return c;
 }
 
@@ -27,23 +28,21 @@ void print_list(struct cookies *c) {
   }
 }
 
+struct node * insert_front(struct cookies *c, char *t, int n) {
+  struct cookies *new = makecookies(t, n, c);
+  return new;
+}
+
 int main() {
   int i = 0;
   srand(time(NULL));
-  for (i = 0; i < 3; i ++) {
-    char type[15] = {'h', 'i'};
-    struct cookies *c = makecookies(type, 3);
-    // printcookies(c);
-    free(c);
-  }
 
   struct cookies *test;
   for (i = 0; i < 3; i ++) {
     char type[15] = {'b'};
-    struct cookies *test = makecookies(type, rand() % 10);
-    print_list(test);
-    free(test);
+    test = insert_front(test, type, rand() % 10);
   }
+  print_list(test);
 
 
   return 0;
