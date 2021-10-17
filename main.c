b17798230562da9d341d9ba1c3c01d393034d1d7
jennisun <jsun20@stuy.edu>
2021-10-17 10:31:06 -0400

    free_list

--- main.c
+++ main.c
@@ -28,22 +28,35 @@ void print_list(struct cookies *c) {
   }
 }
 
-struct node * insert_front(struct cookies *c, char *t, int n) {
+struct cookies * insert_front(struct cookies *c, char *t, int n) {
   struct cookies *new = makecookies(t, n, c);
   return new;
+}
+
+struct cookies * free_list(struct cookies *c) {
+  struct cookies *next = c -> next;
+  while (next) {
+    free(c);
+    c = next;
+    next = c -> next;
+  }
+  return next;
 }
 
 int main() {
   int i = 0;
   srand(time(NULL));
 
-  struct cookies *test;
+  struct cookies *test = 0;
   for (i = 0; i < 3; i ++) {
-    char type[15] = {'b'};
+    char type[15] = {rand() % 26 + 'a', rand() % 26 + 'a', rand() % 26 + 'a'};
     test = insert_front(test, type, rand() % 10);
   }
   print_list(test);
 
+  printf("\nPrint free_list: \n");
+  test = free_list(test);
+  print_list(test);
 
   return 0;
 }
