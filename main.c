f145ba00f7fc5097b8e6dd1e30b3f25dd7207e7f
jennisun <jsun20@stuy.edu>
2021-10-18 20:54:50 -0400

    wrote remove_cookies

--- main.c
+++ main.c
@@ -43,6 +43,27 @@ struct cookies * free_list(struct cookies *c) {
   return next;
 }
 
+struct cookies * remove_cookies(struct cookies *front, char *t, int n) {
+  struct cookies *start = front;
+  struct cookies *temp = front -> next;
+
+  if (!strcmp(front -> type, t) && front -> num == n) {
+    return temp;
+  }
+
+  while (temp -> next) {
+    if (!strcmp(temp -> type, t) && temp -> num == n) {
+      front -> next = temp -> next;
+      // printcookies(front);
+      break;
+    }
+    front = temp;
+    temp = temp -> next;
+
+  }
+  return start;
+}
+
 int main() {
   int i = 0;
   srand(time(NULL));
@@ -52,6 +73,17 @@ int main() {
     char type[15] = {rand() % 26 + 'a', rand() % 26 + 'a', rand() % 26 + 'a'};
     test = insert_front(test, type, rand() % 10);
   }
+  char type[3] = {'h', 'e'};
+  test = insert_front(test, type, 2);
+
+  char type1[3] = {'w', 'r'};
+  test = insert_front(test, type1, rand() % 10);
+
+  print_list(test);
+
+  printf("\n");
+  test = remove_cookies(test, type, 2);
+  printf("\n");
   print_list(test);
 
   printf("\nPrint free_list: \n");
